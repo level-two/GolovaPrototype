2bdf6ea2aa2d175b2e76a48df04eacab711aee96
Yauheni Lychkouski <ylychkouski@productiveedge.com>
2019-06-07 18:38:42 +0300

    Draft

--- GolovaPrototype/GameScene.swift
+++ GolovaPrototype/GameScene.swift
@@ -10,34 +10,76 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     override func didMove(to view: SKView) {
-        
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+//        if let label = self.label {
+//            label.alpha = 0.0
+//            label.run(SKAction.fadeIn(withDuration: 2.0))
+//        }
         
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//        let w = (self.size.width + self.size.height) * 0.05
+//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+//        if let spinnyNode = self.spinnyNode {
+//            spinnyNode.lineWidth = 2.5
+//
+//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                              SKAction.fadeOut(withDuration: 0.5),
+//                                              SKAction.removeFromParent()]))
+//        }
+        head = .init(imageNamed: "head.png")
+        defaultHeadSize = head.size
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
+//
+//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+//        touches
+//            .filter { $0.location(in: self.view).fallsIn(head.frame) }
+//            .forEach { $0. }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
+        let locations = touches.map { $0.location(in: self) }
+        let center = locations.reduce(CGPoint.zero, +)/CGFloat(touches.count)
+        
+        head.position = center
+        
+        if locations.count == 2 {
+            let gap = locations.max { $0.y > $1.y }!.y - locations.min { $0.y < $1.y }!.y
+            head.size = CGSize(width: head.size.width, height: gap)
+        } else {
+            head.size = defaultHeadSize
         }
     }
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
     
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    var head: SKSpriteNode!
+    var defaultHeadSize: CGSize = .zero
+}
+
+/*
+extension GameScene {
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -61,29 +103,25 @@ class GameScene: SKScene {
             self.addChild(n)
         }
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+}
+ */
+
+//extension CGPoint {
+//    func fallsIn(_ rect: CGRect) -> Bool {
+//        return
+//            self.x >= rect.minX &&
+//            self.x <= rect.maxX &&
+//            self.y >= rect.minY &&
+//            self.y <= rect.maxY
+//    }
+//}
+
+extension CGPoint {
+    static func +(_ a: CGPoint, _ b: CGPoint) -> CGPoint {
+        return CGPoint(x: a.x + b.x, y: a.y + b.y)
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    static func /(_ a: CGPoint, _ b: CGFloat) -> CGPoint {
+        return CGPoint(x: a.x / b, y: a.y / b)
     }
 }
--- GolovaPrototype/GameViewController.swift
+++ GolovaPrototype/GameViewController.swift
@@ -15,21 +15,16 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        guard
+            let view = self.view as! SKView?,
+            let scene = SKScene(fileNamed: "GameScene")
+            else { return }
+        
+        scene.scaleMode = .aspectFill
+        view.ignoresSiblingOrder = true
+        view.showsFPS = true
+        view.showsNodeCount = true
+        view.presentScene(scene)
     }
 
     override var shouldAutorotate: Bool {
